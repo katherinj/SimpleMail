6d06deaeb54546415c37888e6cb54559a266a000
Katherin Jimenez <katjimenez98@gmail.com>
2025-02-10 16:34:44 -0500

    Step 5: Bind the adapter to the RecyclerView

--- app/src/main/java/com/example/simplemail/MainActivity.kt
+++ app/src/main/java/com/example/simplemail/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var emails: List<Email>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,14 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val emailsRecyclerView = findViewById<RecyclerView>(R.id.emailsRv)
+        emails = EmailFetcher.getEmails()
+        val emailAdapter = EmailAdapter(emails)
+
+        emailsRecyclerView.adapter = emailAdapter
+
+        emailsRecyclerView.layoutManager = LinearLayoutManager(this)
+
     }
 }
